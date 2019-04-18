c0a328eeccfa49cefb881c9384073211bddb0b92
unknown <antonioestival@gmail.com>
2019-04-18 17:35:17 -0300

    correção de rota raiz

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json())
 
 const port = process.env.PORT || 3000
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({ "msg": "{Up & Runnig" })
 })
 
