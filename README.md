9862a1e650dcaeef5cc9b88da3775e767da0ddcf
unknown <antonioestival@gmail.com>
2019-04-18 17:32:15 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,11 @@
+# Objetivo
+
+Wirecard Mock Service for WebHook
+
+
+## Deploy on Heroku
+
+You can deploy your own copy of the app using the web-based flow:
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/antonioestival/wirecard-mock-app)
+
---
+++ src/index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 3000
+
+app.post('/', (req, res) => {
+  res.status(200).json({ "msg": "{Up & Runnig" })
+})
+
+app.post('/order', (req, res) => {
+  console.log("\n\n\n=========================================================================================== ")
+  console.log("====== req.headers: ")
+  console.log(req.headers)
+  console.log("====== req.params: ")
+  console.log(req.params)
+  console.log("====== req.body: ")
+  console.log(req.body)  
+  res.status(200).json("OK")
+})
+
+app.post('/payment', (req, res) => {
+  console.log("\n\n\n=========================================================================================== ")
+  console.log("====== req.headers: ")
+  console.log(req.headers)
+  console.log("====== req.params: ")
+  console.log(req.params)
+  console.log("====== req.body: ")
+  console.log(req.body)
+
+  res.status(200).json("OK")
+})
+
+app.post('/refund', (req, res) => {
+  console.log("\n\n\n=========================================================================================== ")
+  console.log("====== req.headers: ")
+  console.log(req.headers)
+  console.log("====== req.params: ")
+  console.log(req.params)
+  console.log("====== req.body: ")
+  console.log(req.body)
+
+  res.status(200).json("OK")
+})
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+  console.log('To shutdown the server: ctrl + c')
+})
